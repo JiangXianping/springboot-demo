2c9539a391ac301c20411c461f07657ef666a9b0
jiangxianping <992386253@qq.com>
2019-03-28 11:14:01 +0800

    @Cacheable Other properties use

--- src/main/java/com/jiang/springboot/service/EmployeeService.java
+++ src/main/java/com/jiang/springboot/service/EmployeeService.java
@@ -17,16 +17,19 @@ public class EmployeeService {
      *
      * CacheManager管理多个Cache组件的，对缓存的真正CRUD操作在Cache组件中，每一个缓存组件有自己唯一一个名字
      * 几个属性：
-     *          cacheNames/value：指定缓存的名字
+     *          cacheNames/value：指定缓存的名字；将方法的返回结果放在哪个缓存中，是数组的方式，可以指定多个缓存。
      *          key：缓存数据使用的key：可以用它来指定。默认是使用方法参数的值 1-方法的返回值
      *              编写SpEL：#id;参数id的值 #a0 #p0 #root.args[0]
-     *          keyGenerator：key的生成器；可以自己指定key的生成器的组件id
+     *
+     *          keyGenerator：key的生成器   getEmp[1]；可以自己指定key的生成器的组件id
      *              keyGenerator/key：二选一使用
      *          cacheManager：指定缓存管理器：或者cacheResolver指定获取解析器
      *          condition：指定符合条件的情况下才缓存
+     *                  ，condition="#id>0"
+     *                  condition ="#id>1"；第一个参数的值>1的时候才进行缓存
      *          unless：否定缓存：当unless指定的条件为true，方法的返回值就不会被缓存
      *                  unless = "#result == null"
-     *          sync：是否使用异步模式
+     *          sync：是否使用异步模式 （unless不可用）
      *  原理：
      *      1.自动配置类：
      *      2.缓存的配置类
@@ -68,11 +71,10 @@ public class EmployeeService {
      * @param id
      * @return
      */
-    @Cacheable(cacheNames = {"emp"})
+    @Cacheable(cacheNames = {"emp"},keyGenerator = "myKeyGenerator",condition = "#id>1",unless = "#a0==2")
     public Employee getEmp(Integer id){
         System.out.println("查询："+id+"号员工");
         Employee emp = employeeMapper.getEmpById(id);
         return  emp;
     }
-
 }
